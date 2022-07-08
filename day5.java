f38fffc72058bc1ea7c368e4a088c47a53651de6
Alex Meng <36436765+alexumeng@users.noreply.github.com>
2022-07-08 16:32:06 -0700

    My notes for Day 5

---
+++ day5.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+import javax.security.auth.x500.X500Principal;
+
+public class day5
+{
+    public static void main(String []args)
+    {
+        
+        // homework review: write a for loop from 0 to 1000 that steps by 20, translate into while loop
+
+        // review -- if statements, what's wrong with these code blocks?
+
+        if x > 0 then system.out.println(x);
+
+        if (x > 0)
+        {
+            System.out.println(x);
+        }
+
+        int x = 0;
+        int y = 40;
+        if (1 + x > Math.pow(x, Math.sqrt(2))) {
+            y = y + x; 
+            }
+            
+        if (x == 1) { 
+            y++; 
+        }
+
+        int sum = 0;
+        x = in.nextInt();
+        if (in.hasNextInt(x)){
+            sum = sum + x;
+        }
+        else
+        {
+            System.out.println("Bad input for x");
+        }
+
+        String letterGrade = "F";
+        int grade = 60;
+        if (grade >= 90) { 
+            letterGrade = "A"; 
+        }
+        if (grade >= 80) { 
+            letterGrade = "B"; 
+        }
+        if (grade >= 70) {
+            letterGrade = "C"; 
+        }
+        if (grade >= 60) {
+            letterGrade = "D"; 
+        }
+
+
+        // code time
+        // fizzbuzz
+        // write a program that prints each number from 1 to 100 on a new line.
+        // for each multiple of 3, print "Fizz" instead of the number
+        // for each multiple of 5, print "Buzz" instead of the number
+        // for numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number
+        // for number that are none of the above, simply print the number
+
+        // hint, you'll need to use modulus for this (%). This divdes the two numbers and returns the REMAINDER
+
+
+
+        // write a program that reads a number and prints if its positive, negative, or zero.
+
+
+
+
+        // write a program that reads 3 numbers and prints if they're in increasing or decreasing order, and neither otherwise
+        // Increasing = strictly increasing. If there is a repeat such as 3 4 4, then that is not increasing. 
+        
+
+
+
+    }
+}
