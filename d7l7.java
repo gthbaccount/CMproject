3bcec4b4ee96fefc54cb62df5a41e900ad8c6117
gthbaccount <107728322+gthbaccount@users.noreply.github.com>
2022-07-29 16:27:24 -0700

    Add files via upload

---
+++ d7l7.java
@@ -0,0 +1,32 @@
+import java.lang.Math;
+public class d7l7 {
+    public static void main(String []args){
+        // for(int i = 10; i > 0; i--){
+        //     for(int x = 5; x > 0; x--){
+        //         System.out.print("x");
+        //     }
+        //     System.out.println("");
+        // } 
+        // int counter = 1;
+        // int sum = 0;
+        // for(int y = 333; y <= 378; y++){
+        //     sum = y + sum;
+        //     counter++;
+        // }
+        // System.out.println(sum / counter);
+        
+        // int x = 0;
+        // while(x <= 50){
+        //     System.out.println(Math.pow(2, x));
+        //     x++;
+        // }
+           
+        
+        for(int x = 1; x < 5; x++){
+            for(int z = 0; z < x; z++){
+                System.out.print("[]");
+            }
+            System.out.println("");
+        }
+        }
+    }
