6e0d554cbc3db1cc1abd4dcbf65600432d352e0a
gthbaccount <107728322+gthbaccount@users.noreply.github.com>
2022-08-12 16:36:44 -0700

    Add files via upload

---
+++ d9l9.java
@@ -0,0 +1,22 @@
+public class d9l9 {
+    public static int mystery(int n){
+        ++n;
+        ++n;
+        return n;
+    }
+
+    public static double ok(double input1, double input2){
+        double output = input1 + input2;
+        return output;
+    }
+
+    public static void main(String[] args){
+        int a = 5;
+        System.out.println(ok(2.0, 3.0));
+        System.out.println(mystery(a));
+    }
+
+    
+
+}
+
