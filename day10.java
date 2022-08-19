317d616e88106cbf7ab2e2bcd9b6497454aab1df
Alex Meng <36436765+alexumeng@users.noreply.github.com>
2022-08-19 16:31:28 -0700

    day 10 notes

---
+++ day10.java
@@ -0,0 +1,81 @@
+import java.util.ArrayList;
+
+public class day10 {
+    public static void main(String[] args)
+    {
+        // homework frm last week
+        // create a method that adds two numbers together 
+        // call it in your main function
+
+        
+        // create 2 methods
+
+        // method 1
+        // takes in 2 numbers, subtracts them
+        
+        // method 2 (int parameter1, int parameter2)
+        // takes in 2 numbers, multiplies them together
+        // in this method, call method 1, pass in the two numbers
+        // passed to it, and save it's return value = subtractResult
+        // use that return value & multiply that result with it's 2nd parameter
+        // subtractResult * parameter2
+        // return that ^
+
+        // 5-3 implementing methods
+
+        // Let's work together to create a method that can convert kilometers to miles. 
+        // There's a few steps and things that will help us get there:
+
+        // Describe how the method works and what it should do
+        // This method converts kilometers to miles
+        // Determine what inputs are needed and what returns are expected
+        // Kilometers will be inputted and miles will be returned
+        // Determine what data types the inputs and outputs are
+        // Kilometers will be an int
+        // Miles will be a double
+        // That means the return type should be double, that is "public static double kmconvert()"
+        // Write pseudocode for how to obtain the desired result (figure out how to get the answer)
+
+        // 1 kilometer is equal to 0.62 miles
+        // So given n kilometers, the conversion will be n*0.62 miles
+        // Return n*0.62
+        // Write the code! Don't forget the return!
+
+        // ***
+        // What does this method return for n = 1, n = 2, n = 17, n= 21? 
+
+        // public static boolean mystery (int n){
+        //     if (n % 2 == 0) { return true };
+        //     else { return false; }
+        // }
+        
+        // ***
+        // Create a method that checks to see whether or not a number is divisible by 17. Here are some things to consider:
+
+        // What are your parameters (inputs)?
+        // What are you returning (outputting)?
+        // What type is that return?
+
+        // ***
+        // Create a method that sums up all the numbers from 0, 1, 2 .. n-1, n. That is, add up all the numbers from 0 up until the parameter n.
+
+        // For example, if n = 5, the answer is 1+2+3+4+5 = 15.
+
+        // ***
+        // Create a method that accepts a String as a parameter then:
+
+        // Appends "ly" to the String if its length is larger than 4
+        // Appends "ing" to the String if its length is less than 4
+        // Finally, returns the String
+        // Hint: Remember the string.length() method
+
+        // calculator project
+
+        // ArrayList<Integer> list = new ArrayList<Integer>();
+        
+        
+
+        
+    }
+
+}
