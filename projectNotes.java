ea2a4701a9d357b7c5cd85f9bc02a962d06591bd
Alex Meng <36436765+alexumeng@users.noreply.github.com>
2022-08-26 16:32:58 -0700

    My notes as we go through the project

---
+++ projectNotes.java
@@ -0,0 +1,57 @@
+// JavaScript source code
+import java.util.ArrayList;
+
+public class projectNotes {
+    public static void main(String[] args) {
+
+        // calculator project
+
+
+        userInput = "5+5-2"
+
+        5 + 5 - 2
+        01234
+
+        "-"
+
+        userInput.length()
+
+        userInput.charAt(x)
+
+
+
+
+        "21-3-2"
+
+
+        num = ""
+        prev = ""
+
+        loop {
+            // loop 0
+            char = "2"
+            prev = "2"
+            // loop 1
+            char = "1"
+            prev = "2" + "1" = "21"
+            // loop 2
+            char = "-"
+            // check num
+            // reset previous
+            num = prev = "21"
+            prev = ""
+
+            // loop 3
+            char = "3"
+            prev = "3"
+            // loop 4
+            char = "-"
+            // check num
+            // num is not empty --> so we will do arithmetic
+            // reset previous
+        }
+
+
+    }
+
+}
