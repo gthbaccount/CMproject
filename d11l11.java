b3537305f372dfa3daf82218e4712d2712530de5
gthbaccount <107728322+gthbaccount@users.noreply.github.com>
2022-09-09 16:31:10 -0700

    Add files via upload

--- d11l11.java
+++ d11l11.java
@@ -12,30 +12,69 @@ public class d11l11 {
         return output;
         //amogus
     }
+    public static double subtraction(String num1, String num2){
+        double output = 0;
+        output = Double.parseDouble(num1) - Double.parseDouble(num2);
+        return output;
+    }
+    
     public static void main(String[] args){
         
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Put in your equation.");
-        String userInput = scanner.nextLine();
-        for(int x = 0; x < userInput.length(); x++){
-            System.out.println(userInput.charAt(x));
 
-            if(Character.toString(userInput.charAt(x)) == "-"){
-
+        try
+        {
+            System.out.println("Put in your equation.");
+            String userInput = scanner.nextLine();
+            String num = "";
+            String prev = "";
+            double output = 0;
+            for(int x = 0; x < userInput.length(); x++){
+                //System.out.println(userInput.charAt(x));
+                int count = 1;
+                String math = "";
+                if(userInput.charAt(x) == '-'){
+                    System.out.println(prev);
+                    while(userInput.charAt(x+ count) != '-' ||
+                    userInput.charAt(x+ count) != '+' ||
+                    userInput.charAt(x+ count) != '/' ||
+                    userInput.charAt(x+ count) != '*'){
+                        math += userInput.charAt(x+count);
+                        count++;
+                        if(x + count >= userInput.length()){
+                            break;
+                        }
+                      }
+                      output = subtraction(prev, math);
+                      x += count;
+                      prev = "";
+                      System.out.print(output);
+                }
+                else if(userInput.charAt(x) == '+'){
+                    prev = "";
+                }
+                else if(userInput.charAt(x) == '/'){
+                    prev = "";
+                }
+                else if(userInput.charAt(x) == '*'){
+                    prev = "";
+                }
+                else{
+                    if(num == ""){
+                        num = Character.toString(userInput.charAt(x));
+                        prev = Character.toString(userInput.charAt(x));
+                    }
+                    else{
+                        num = Character.toString(userInput.charAt(x));
+                        prev = prev + num;
+                    }
+                   
+                }
+               
             }
-            else if(Character.toString(userInput.charAt(x)) == "+"){
-
-            }
-            else if(Character.toString(userInput.charAt(x)) == "/"){
-
-            }
-            else if(Character.toString(userInput.charAt(x)) == "*"){
-
-            }
-            else{
-
-            }
-           
+        }
+        finally{
+            scanner.close();
         }
         
     }
